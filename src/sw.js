d265e624a04de85a50ee0e9c1be65242d08b2a2b
Yong Sheng Tan <tanys@outlook.sg>
2020-06-05 18:45:54 +0800

    Fixed build bugs

--- src/sw.js
+++ src/sw.js
@@ -1,28 +1,3 @@
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-
-const isNav = event => event.request.mode === "navigate";
-
-workbox.routing.registerRoute(({ url, event }) => {
-  return url.pathname === "/";
-}, workbox.strategies.cacheFirst());
-
-workbox.routing.registerRoute(
-  ({ event }) => isNav(event),
-  new workbox.strategies.NetworkFirst({
-    cacheName: workbox.core.cacheNames.precache,
-    networkTimeoutSeconds: 5,
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [200]
-      })
-    ]
-  })
-);
-
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.setCatchHandler(({ event }) => {
-  if (isNav(event))
-    return caches.match(workbox.precaching.getCacheKeyForURL("/index.html"));
-  return Response.error();
-});
+import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw/';
+setupRouting();
+setupPrecaching(getFiles());
